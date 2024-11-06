16ede4a89dc8aad476ea12faba0145f59bd4510b
Emi160310 <hlin51436@gmail.com>
2024-11-06 12:17:00 +0900

    その３のviewから

--- event/models.py
+++ event/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    event_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.events_text
